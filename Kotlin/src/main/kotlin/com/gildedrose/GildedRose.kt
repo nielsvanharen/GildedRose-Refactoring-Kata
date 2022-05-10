a49efa8913337ae4fbb9b00a8a6f09384f0a6504
Niels van Haren <niels@response.nl>
2022-05-10 13:01:48 +0200

    create factory class
    create factory class

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,22 +5,19 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
-            if (items[i].name.equals("Aged Brie")) {
-                items[i] = AgedBrie(items[i]).updateItem().toItem()
-            }
-            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                items[i] = BackStagePass(items[i]).updateItem().toItem()
-            }
-            if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i] = HandOfRagnaros(items[i]).updateItem().toItem()
-            }
-            if (items[i].name.equals("Elixir of the Mongoose")) {
-                items[i] = CustomizableItem(items[i]).updateItem().toItem()
-            }
-            if (items[i].name.equals("Conjured Mana Cake")) {
-                items[i] = ConjuredItem(items[i]).updateItem().toItem()
-            }
+            items[i] = ItemFactory.createCustomizableItem(items[i]).updateItem().toItem()
         }
+    }
+}
+
+//static object
+object ItemFactory {
+    fun createCustomizableItem(item: Item): CustomizableItem = when (item.name) {
+        "Aged Brie" -> AgedBrie(item)
+        "Sulfuras, Hand of Ragnaros" -> HandOfRagnaros(item)
+        "Conjured Mana Cake" -> ConjuredItem(item)
+        "Backstage passes to a TAFKAL80ETC concert" -> BackStagePass(item)
+        else -> CustomizableItem(item)
     }
 }
 
